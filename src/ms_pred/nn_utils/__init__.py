76b0444c2def109d4a67ce229ab75aadedd2cee3
Ralf Weber <RJMW@users.noreply.github.com>
2026-09-15 10:12:37 +0100

    Make ray imports in nn_utils optional

--- src/ms_pred/nn_utils/__init__.py
+++ src/ms_pred/nn_utils/__init__.py
@@ -1,7 +1,15 @@
 from .nn_utils import *
-from .tune_utils import *
+try:
+    from .tune_utils import *
+except ModuleNotFoundError as err:
+    if err.name != "ray":
+        raise
 from .mol_graph import *
-from .base_hyperopt import *
+try:
+    from .base_hyperopt import *
+except ModuleNotFoundError as err:
+    if err.name != "ray":
+        raise
 from .transformer_layer import *
 from .form_embedder import *
 from .dgl_graph_ops import *
