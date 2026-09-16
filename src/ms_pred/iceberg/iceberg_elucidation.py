67ba0f202305d4bed3134991dd4562fe1edb1763
Ralf Weber <RJMW@users.noreply.github.com>
2026-09-16 23:06:12 +0100

    Fix running ICEBERG from an installed package (#41)
    * Locate predict_smis.py relative to the package
    
    * Use pl.seed_everything

--- src/ms_pred/iceberg/iceberg_elucidation.py
+++ src/ms_pred/iceberg/iceberg_elucidation.py
@@ -334,7 +334,7 @@ def iceberg_prediction(
         df.to_csv(save_dir / f'cands_df_{exp_name}.tsv', sep='\t', index=False)
 
         # run iceberg to generate in-silico spectrum
-        cmd = (f'''{python_path} src/ms_pred/iceberg/predict_smis.py \\
+        cmd = (f'''{python_path} {Path(__file__).resolve().parent / "predict_smis.py"} \\
                --batch-size {batch_size} \\
                --num-gpu-workers {num_gpu_workers} \\
                --num-cpu-workers {num_cpu_workers} \\
